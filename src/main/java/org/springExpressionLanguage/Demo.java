512ce97a94822504750f8cb7d8282892d3774263
mkharshita <mkharshita5@gmail.com>
2024-09-15 22:43:28 +0530

    invoke static method, variables and creating object

--- src/main/java/org/springExpressionLanguage/Demo.java
+++ src/main/java/org/springExpressionLanguage/Demo.java
@@ -10,6 +10,15 @@ public class Demo {
     @Value("#{1+2}")
     private int y;
 
+    @Value("#{T(java.lang.Math).sqrt(25)}")
+    private double z;
+
+    @Value("#{T(java.lang.Math).E}")
+    private double e;
+
+    @Value("#{new java.lang.String('Harshita Saxena')}")
+    private String name;
+
     public int getX() {
         return x;
     }
@@ -26,11 +35,39 @@ public class Demo {
         this.y = y;
     }
 
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public double getE() {
+        return e;
+    }
+
+    public void setE(double e) {
+        this.e = e;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "Demo{" +
                 "x=" + x +
                 ", y=" + y +
+                ", z=" + z +
+                ", e=" + e +
+                ", name='" + name + '\'' +
                 '}';
     }
+
 }
