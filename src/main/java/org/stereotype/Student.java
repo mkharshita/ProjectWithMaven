0985f9e26bedf00ce4dcc7ea1b8417882ed97411
mkharshita <mkharshita5@gmail.com>
2024-09-15 22:07:37 +0530

    collections stereotype

--- src/main/java/org/stereotype/Student.java
+++ src/main/java/org/stereotype/Student.java
@@ -3,13 +3,19 @@ package org.stereotype;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component("myStudent")
 public class Student {
 
     @Value("Harshita")
     private String studentName;
+
     @Value("Moradabad")
     private String city;
+
+    @Value("#{mybestfriends}")
+    private List<String> bestFriends;
 
     public String getStudentName() {
         return studentName;
@@ -27,11 +33,21 @@ public class Student {
         this.city = city;
     }
 
+    public List<String> getBestFriends() {
+        return bestFriends;
+    }
+
+    public void setBestFriends(List<String> bestFriends) {
+        this.bestFriends = bestFriends;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
                 "studentName='" + studentName + '\'' +
                 ", city='" + city + '\'' +
+                ", bestFriends=" + bestFriends +
                 '}';
     }
+
 }
